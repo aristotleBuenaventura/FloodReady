4c7cd198f847aa408fa15fc54eb5286597398f41
Aristotle Buenaventura <aaristotlee@gmail.com>
2023-08-28 12:49:06 +0800

    Added the pinch and grip animation for both hands

--- FloodReady/Assets/Codes/AnimateHandOnInput.cs
+++ FloodReady/Assets/Codes/AnimateHandOnInput.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
     }
 }
