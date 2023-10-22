852e7f13a84c3dd6b00ad208254989da37276500
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2023-10-22 14:46:13 +0800

    go bag working 90%

--- FloodReady/Assets/Codes/InventoryManager.cs
+++ FloodReady/Assets/Codes/InventoryManager.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 public class InventoryManager : MonoBehaviour
 {
     public List<string> itemList = new List<string>();
-    public string[] bagInventory = new string[10] { "Canned good", "Money", "Bottled water", null, null, null, null, null, null, null };
+    public string[] bagInventory = new string[10] { "Canned good", "Money", "Bottled water", "First aid kit", null, null, null, null, null, null };
 
     // Detect collision and remove items from the bag's inventory array
     void OnCollisionEnter(Collision collision)
     {
         string collidedObjectName = collision.gameObject.name;
 
-        if (collidedObjectName == "Canned good" || collidedObjectName == "Money" || collidedObjectName == "Bottled water")
+        if (collidedObjectName == "Canned good" || collidedObjectName == "Money" || collidedObjectName == "Bottled water" || collidedObjectName == "First aid kit")
         {
             // Find the index of the collided item in the bag's inventory
             int itemIndex = System.Array.IndexOf(bagInventory, collidedObjectName);
