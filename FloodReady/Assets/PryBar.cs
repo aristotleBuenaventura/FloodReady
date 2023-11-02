0582652df3bca85cd71a97c238f7e3c291fb0e4e
Adimno <jayjaynaval02@gmail.com>
2023-11-02 15:29:56 +0800

    Fixed the error of prybag and make it able to crush the glass using prybar only and integrated the tags

---
+++ FloodReady/Assets/PryBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PryBar : MonoBehaviour
+{
+    public OVRGrabber leftGrabber; // Assign the left OVRGrabber component in the Unity Editor
+    public OVRGrabber rightGrabber; // Assign the right OVRGrabber component in the Unity Editor
+
+    void Update()
+    {
+        // Check if the user is holding the PryBar using either the left or right OVRGrabber
+        if ((leftGrabber != null && leftGrabber.grabbedObject == GetComponent<OVRGrabbable>()) ||
+            (rightGrabber != null && rightGrabber.grabbedObject == GetComponent<OVRGrabbable>()))
+        {
+            // Check for interactions with BreakableWindow objects
+            Collider[] colliders = Physics.OverlapBox(transform.position, transform.lossyScale / 2f, transform.rotation);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("BreakableWindow"))
+                {
+                    // Ensure the PryBar is the only object that can break the window
+                    if (gameObject.name == "PryBar")
+                    {
+                        BreakableWindow window = collider.GetComponent<BreakableWindow>();
+                        if (window != null && !window.isBroken)
+                        {
+                            window.breakWindow();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
