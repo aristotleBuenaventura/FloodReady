43795e269b37738f84dcea606d4b742fc17a5c6b
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2023-11-14 16:15:12 +0800

    water rising fix

--- FloodReady/Assets/Codes/WaterLevelController.cs
+++ FloodReady/Assets/Codes/WaterLevelController.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class WaterLevelController : MonoBehaviour
 {
-    public Timer timer; // Reference to your Timer script
-    public Rigidbody waterRigidbody; // Reference to the Rigidbody component of the water object
-    public float maxHeight = 5.0f; // Adjust this value to the maximum height of the water
-    public float riseSpeed = 1.0f; // Adjust the speed at which the water rises
+    public float risingSpeed = 0.1f; // Adjust the speed as needed
+    private float nextRiseTime;
+    private float targetWaterLevel; // New variable to store the desired water level
+
+    private void Start()
+    {
+        nextRiseTime = Time.time + 1f; // Start rising after 1 second
+        targetWaterLevel = transform.position.y; // Set initial water level to current position
+    }
 
     void Update()
     {
-        if (timer != null && waterRigidbody != null)
+        if (Time.time >= nextRiseTime)
         {
-            // Calculate the new water level based on the timer's remaining time
-            float timerPercentage = timer.RemainingTime / timer.InitialTime; // Assuming you have an InitialTime property in Timer
-            float newWaterY = transform.position.y + maxHeight * (1 - timerPercentage);
-
-            // Calculate the distance to rise
-            float distanceToRise = newWaterY - waterRigidbody.position.y;
-
-            // Apply force to the Rigidbody to move the water
-            Vector3 force = Vector3.up * (distanceToRise * riseSpeed);
-
-            waterRigidbody.AddForce(force, ForceMode.Force);
+            RiseWater();
         }
+    }
+
+    void RiseWater()
+    {
+        targetWaterLevel += risingSpeed; // Adjust the target water level
+
+        transform.position = new Vector3(transform.position.x, targetWaterLevel, transform.position.z);
+
+        // Ensure rotation remains zero for all axes
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        nextRiseTime += 1f; // Set the next rise time
     }
 }
