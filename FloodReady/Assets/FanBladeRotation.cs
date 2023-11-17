bb92da31cc71f7bc486f8d2f50a2d2f274acef22
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2023-11-17 18:45:08 +0800

    fix the plug position when play

--- FloodReady/Assets/FanBladeRotation.cs
+++ FloodReady/Assets/FanBladeRotation.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class RotateVRObject : MonoBehaviour
 {
     public float rotationSpeed = 1000.0f;
+    public bool shouldRotate = true;
 
     void Update()
     {
-        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        if (shouldRotate)
+        {
+            transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        }
     }
 }
--- FloodReady/Assets/PlugSocketAttachment.cs
+++ FloodReady/Assets/PlugSocketAttachment.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class PlugSocketAttachment : MonoBehaviour
 {
-    private bool isAttached = false;
+    public bool isAttached { get; private set; } = false;
+
+    // Public variables for manual position and rotation adjustment
+    public Vector3 manualPosition = Vector3.zero;
+    public Vector3 manualRotation = Vector3.zero;
+
+    private Vector3 originalLocalPosition;
+    private Quaternion originalLocalRotation;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,13 +25,40 @@ public class PlugSocketAttachment : MonoBehaviour
         isAttached = true;
 
         // Disable physics interactions or apply any other necessary adjustments
-        GetComponent<Rigidbody>().isKinematic = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+
+        // Store the current local position and rotation of the plug
+        originalLocalPosition = transform.localPosition;
+        originalLocalRotation = transform.localRotation;
 
         // Attach the plug to the socket
         transform.SetParent(socket);
 
-        // Optional: Adjust the position and rotation if needed
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.identity;
+        // Set the local position and rotation to the manual values
+        transform.localPosition = manualPosition;
+        transform.localRotation = Quaternion.Euler(manualRotation);
+    }
+
+    public void DetachFromSocket()
+    {
+        // Restore the original local position and rotation
+        transform.localPosition = originalLocalPosition;
+        transform.localRotation = originalLocalRotation;
+
+        // Enable physics interactions or apply any other necessary adjustments
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+
+        // Detach the plug from the socket
+        transform.SetParent(null);
+
+        isAttached = false;
     }
 }
