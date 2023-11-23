2614143efd28fc7adf9a47a7704471f12bd5cf12
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2023-11-23 20:16:54 +0800

    Created the life when the water is lower than  character life increases and if water is higher life decreases

--- FloodReady/Assets/HealthBar.cs
+++ FloodReady/Assets/HealthBar.cs
@@ -7,10 +7,13 @@ public class HealthBar : MonoBehaviour
 {
     public float maxLife = 100f;  // Maximum life value
     public float lifeDecreaseRate = 1f;  // Rate at which life decreases per second
+    public float lifeIncreaseRate = 1f;  // Rate at which life increases per second
     private float currentLife;  // Current life value
     private Image healthbar;  // Reference to the health bar image component
 
     public GameObject healthBarObject;  // Assign the health bar GameObject in the Unity Editor
+    public GameObject player; // Assign the player GameObject (OVRPlayerController) in the Unity Editor
+    public GameObject water; // Assign the water GameObject in the Unity Editor
 
     // Start is called before the first frame update
     void Start()
@@ -30,18 +33,27 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Decrease life over time
-        Debug.Log("Life decreases");
-        currentLife -= lifeDecreaseRate * Time.deltaTime;
+        // Check if the player is higher than the water
+        if (water != null && player != null && player.transform.position.y > water.transform.position.y)
+        {
+            // Increase life over time
+            Debug.Log("Life increases");
+            currentLife += lifeIncreaseRate * Time.deltaTime;
+        }
+        else
+        {
+            // Decrease life over time
+            Debug.Log("Life decreases");
+            currentLife -= lifeDecreaseRate * Time.deltaTime;
+        }
 
-        // Clamp life to ensure it doesn't go below 0
+        // Clamp life to ensure it doesn't go below 0 or above maxLife
         currentLife = Mathf.Clamp(currentLife, 0f, maxLife);
 
         // Update the health bar visual representation (fill amount)
         UpdateHealthBarUI();
 
         // You can add additional logic here, such as triggering events when life reaches certain thresholds.
-
         // Example: if (currentLife <= 0) { PlayerDied(); }
     }
 
