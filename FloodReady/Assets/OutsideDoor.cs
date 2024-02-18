2b65c58f9a67de6f44916d6afd1d8b7ce86b3437
Jayjay Naval <jayjaynaval02@gmail.com>
2024-02-19 04:14:16 +0800

    Fix the sound effect and doors interaction

--- FloodReady/Assets/OutsideDoor.cs
+++ FloodReady/Assets/OutsideDoor.cs
@@ -7,12 +7,13 @@ public class OpenDoorOnTrigger : MonoBehaviour
     public Vector3 desiredRotation = new Vector3(-0.087f, 180.316f, 0.205f);
     public float rotationSpeed = 90.0f; // Speed of rotation
     public GameObject targetObject; // The object to rotate
+    public string handTag = "Hand"; // Tag for the hand objects
 
     private bool isOpening = false; // Flag to check if door is opening
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(handTag))
         {
             Debug.Log("Player detected! Opening door...");
 
