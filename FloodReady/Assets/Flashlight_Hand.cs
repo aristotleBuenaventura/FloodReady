4caa72b63d74ee6fc54d19a8643a05e637e87018
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-02-24 15:05:33 +0800

    flashlight working

---
+++ FloodReady/Assets/Flashlight_Hand.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight_Hand : MonoBehaviour
+{
+    public GameObject flashlight; // Reference to the GameObject to toggle
+
+    private bool isVisible = false;
+
+    // Update is called once per frame
+    void Start()
+    {
+        flashlight.SetActive(false);
+    }
+
+    void Update()
+{
+    // Check for button press to toggle visibility
+    if (OVRInput.GetDown(OVRInput.Button.Two)) // Change the button as per your requirement
+    {
+        isVisible = !isVisible;
+        flashlight.SetActive(isVisible); // Set the active state of the flashlight
+        Debug.Log("flashlight");
+    }
+}
+
+}
