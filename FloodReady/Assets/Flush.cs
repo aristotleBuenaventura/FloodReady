3c0a69dff0ffb65f12bcee340a3f62a834e9fdcb
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-03-02 17:38:06 +0800

    DONE scoring and icons for plunge the toilet

--- FloodReady/Assets/Flush.cs
+++ FloodReady/Assets/Flush.cs
@@ -5,18 +5,24 @@ public class Flush : MonoBehaviour
     public MoveScaleAndDestroy moveScaleAndDestroy; // Reference to MoveScaleAndDestroy script
     public MoveAndScaleUpwardAfterDelay cleanWater;
     public AudioSource flushSound; // Reference to the AudioSource component
+    public plungethetoiletIcon check;
+    public TaskPercentage task;
+
+    private bool hasExecuted = false; // Track if the code has already been executed
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the trigger involves an object with the "Plunge" tag
-        if (other.CompareTag("Plunge"))
+        // Check if the trigger involves an object with the "Plunge" tag and if the code has not been executed yet
+        if (other.CompareTag("Plunge") && !hasExecuted)
         {
             Debug.Log("Trigger with Plunge object!");
 
             // Set goSignal to true for both MoveScaleAndDestroy and MoveAndScaleUpwardAfterDelay scripts
             moveScaleAndDestroy.goSignal = true;
             cleanWater.goSignal = true;
-
+            check.SetCheckIconVisible(true);
+            check.SetUncheckIconVisible(false);
+            task.IncrementTaskPercentage(10);
             // Check if flushSound is assigned
             if (flushSound != null)
             {
@@ -27,6 +33,9 @@ public class Flush : MonoBehaviour
             {
                 Debug.LogWarning("Flush sound is not assigned!");
             }
+
+            // Set the flag to true to indicate that the code has been executed
+            hasExecuted = true;
         }
     }
 }
