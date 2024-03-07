46a6f32c0a4534b691a54176f31bc3d14dbc848a
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-08 01:48:21 +0800

    Fix the collider of unplug bug in tv. (there is still bug in electric fan)

--- FloodReady/Assets/UnplugHolder.cs
+++ FloodReady/Assets/UnplugHolder.cs
@@ -7,44 +7,41 @@ public class UnplugHolder : MonoBehaviour
     public GameObject UnplugCableCanvas;
     public Collider collider1;
     public Collider collider2;
-    public Collider collider3;
-    public Collider collider4;
-    public Collider collider5;
 
+
+    // Flag to track if colliders have been enabled
+    private bool collidersEnabled = false;
 
     void Start()
     {
-        // Make sure the colliders are enabled initially
-        collider1.enabled = true;
-        collider2.enabled = true;
-        collider3.enabled = true;
-        collider4.enabled = true;
-        collider5.enabled = true;
-        
+        // Make sure the colliders are disabled initially
+        DisableColliders();
     }
 
     void Update()
     {
-        // Check if the canvas is not enabled
-        if (!UnplugCableCanvas.gameObject.activeSelf)
+        // Check if the canvas is enabled and colliders haven't been enabled yet
+        if (UnplugCableCanvas.gameObject.activeSelf && !collidersEnabled)
         {
-            // Disable the colliders
-            collider1.enabled = true;
-            collider2.enabled = true;
-            collider3.enabled = true;
-            collider4.enabled = true;
-            collider5.enabled = true;
-        
+            // Enable the colliders and set the flag
+            EnableColliders();
+            collidersEnabled = true;
         }
-        else
-        {
-            // Enable the colliders
-            collider1.enabled = false;
-            collider2.enabled = false;
-            collider3.enabled = false;
-            collider4.enabled = false;
-            collider5.enabled = false;
-       
-        }
+    }
+
+    // Function to enable colliders
+    void EnableColliders()
+    {
+        collider1.enabled = true;
+        collider2.enabled = true;
+   
+    }
+
+    // Function to disable colliders
+    void DisableColliders()
+    {
+        collider1.enabled = false;
+        collider2.enabled = false;
+
     }
 }
