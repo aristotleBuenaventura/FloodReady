a2b51013382291fee20ec617981aff15c3e37fc4
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-16 12:49:31 +0800

    Created hide basket and show use phone canvas

---
+++ FloodReady/Assets/CheckPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CheckPlayer : MonoBehaviour
+{
+    public GameObject playerTag;
+    public GameObject objectsToDisable;
+
+    public RecoveryCanvasController UsePhoneCanvas;
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playerTag)
+        {
+            // Check if the object to disable is active before disabling it
+            if (objectsToDisable.activeSelf)
+            {
+                // Disable the object
+                objectsToDisable.SetActive(false);
+                UsePhoneCanvas.UsePhoneCanvas();
+
+            }
+        }
+    }
+}
---
+++ FloodReady/Assets/ClothesHider.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BasketHider : MonoBehaviour
+{
+    public GameObject ShowgatherDirtyclothesCanvas;
+
+    public GameObject objectToEnable1;
+ 
+
+    private bool objectsEnabled = false; // Track if objects are already enabled
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the canvas becomes active during runtime and objects are not already enabled
+        if (ShowgatherDirtyclothesCanvas.gameObject.activeSelf && !objectsEnabled)
+        {
+            // If canvas becomes active and objects are not already enabled, turn on all objects
+            TurnOnObjects();
+        }
+    }
+
+    // Method to turn on all GameObjects
+    public void TurnOnObjects()
+    {
+        if (objectToEnable1 != null)
+            objectToEnable1.SetActive(true);
+
+        // Update the flag to indicate that objects are now enabled
+        objectsEnabled = true;
+    }
+}
