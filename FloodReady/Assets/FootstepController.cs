3a3694133b0b5ea8ea8e7be0ad8478d85a2dc327
Yunice Nicolle Cruz <cruz.yuniza@gmail.com>
2024-03-17 08:10:56 +0800

    Footstep Setup and Converted Boat to Prefab in Scene 2

---
+++ FloodReady/Assets/FootstepController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class FootstepController : MonoBehaviour
+{
+    public AudioSource footstepSound; 
+
+    private OVRPlayerController playerController;
+    private bool isWalking = false;
+    private bool isRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<OVRPlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the player is walking or running based on VR joystick input
+        isWalking = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).magnitude > 0.1f && OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).magnitude < 0.9f;
+        isRunning = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).magnitude >= 0.9f;
+
+        if (isWalking || isRunning)
+        {
+            footsteps();
+        }
+        else
+        {
+            StopFootsteps();
+        }
+    }
+
+    void footsteps()
+    {
+        // Play footstep sound
+        if (!footstepSound.isPlaying)
+        {
+            footstepSound.Play();
+        }
+    }
+
+    void StopFootsteps()
+    {
+        // Stop footstep sound
+        footstepSound.Stop();
+    }
+}
