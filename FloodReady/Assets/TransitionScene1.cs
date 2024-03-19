c2fd9780745753d0c3cf1f0439d2a08d9a96af44
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-19 23:26:10 +0800

    Created transition changer for hand scene 1 proceed

---
+++ FloodReady/Assets/TransitionScene1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScene1 : MonoBehaviour
+{
+
+    public GameObject loadingScreen; // Reference to your loading screen UI element
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            StartCoroutine(LoadScene("Evacuation_Essential"));
+
+        }
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        // Activate the loading screen
+        loadingScreen.SetActive(true);
+
+        // Load the scene synchronously
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Deactivate the loading screen after the scene is loaded
+        loadingScreen.SetActive(false);
+    }
+}
