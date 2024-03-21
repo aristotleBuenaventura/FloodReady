e03247059b3f841c47d1706a4aa91c1b73e9f952
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-21 23:15:35 +0800

    change the logic of close hint scene 2

--- FloodReady/Assets/CloseHintScene2.cs
+++ FloodReady/Assets/CloseHintScene2.cs
@@ -2,82 +2,29 @@ using UnityEngine;
 
 public class CloseHintScene2 : MonoBehaviour
 {
-    public GameObject objectToMonitor1;
-    public GameObject objectToMonitor2;
-    public GameObject objectToMonitor3;
-    public GameObject objectToMonitor4;
-    public GameObject objectToMonitor5;
-    public GameObject objectToMonitor6;
-
-
-
-    public GameObject objectToMonitor9;
-    public GameObject objectToMonitor10;
-    public GameObject objectToMonitor11;
-    public GameObject objectToMonitor12;
-    public GameObject objectToMonitor13;
-    public GameObject objectToMonitor14;
-
-
     public GameObject objectToShow;
-    public ShowHintCanvasScene2 ShowHintCanvasScene2;
-
-    private bool objectShownOnExit = false;
-
+    public ShowHintCanvasScene2 showHintCanvas;
+    public GameObject[] hintIcons;
 
     private void Start()
     {
         objectToShow.SetActive(false);
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        // Check if the collider that exited the trigger is the player or controller
         if (other.CompareTag("Hand"))
         {
-            // Hide the object to show if it is active and it wasn't shown during OnTriggerExit
-            if (objectToShow.activeSelf && !objectShownOnExit)
-            {
-                objectToShow.SetActive(false);
-                Debug.Log("Object to show was hidden during OnTriggerExit");
-            }
-
-            objectToMonitor9.SetActive(false);
-            objectToMonitor10.SetActive(false);
-            objectToMonitor11.SetActive(false);
-            objectToMonitor12.SetActive(false);
-            objectToMonitor13.SetActive(false);
-            objectToMonitor14.SetActive(false);
-
-
-
-
-
-
-            // Hide all canvas elements
-            ShowHintCanvasScene2.HideAllCanvas();
-
-            // Reset the flag
-            objectShownOnExit = false;
-        }
-    }
-
-    private void Update()
-    {
-        // Check if any of the monitored objects are active
-        if (objectToMonitor1.activeSelf || objectToMonitor2.activeSelf || objectToMonitor3.activeSelf || objectToMonitor4.activeSelf ||
-            objectToMonitor5.activeSelf || objectToMonitor6.activeSelf ||  objectToMonitor9.activeSelf)
-        {
-            // Show the object to show
-            objectToShow.SetActive(true);
-            objectShownOnExit = true;
-            Debug.Log("Object to show is active now");
-        }
-        else
-        {
-            // If no monitored objects are active, hide the object to show
             objectToShow.SetActive(false);
-            Debug.Log("Object to show is inactive now");
+            showHintCanvas.HideAllCanvas();
+
+            foreach (GameObject obj in hintIcons)
+            {
+                if (obj != null) // Check if the GameObject reference is not null
+                {
+                    obj.SetActive(false);
+                }
+            }
         }
     }
 }
