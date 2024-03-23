111cce3d3e485cb636f04cf98434d2d51b1aeef7
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-23 14:48:31 +0800

    tv button vr working

--- FloodReady/Assets/Codes/ButtonVR.cs
+++ FloodReady/Assets/Codes/ButtonVR.cs
@@ -1,55 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class ButtonVR : MonoBehaviour
 {
-
     public GameObject button;
     public UnityEvent onPress;
     public UnityEvent onRelease;
-    GameObject presser;
+    public TextMeshProUGUI buttonText;
+    public MeshRenderer videoCanvas;
+    public VideoPlayer videoPlayer;
     AudioSource sound;
-    bool isPressed;
+    bool isPlaying;
 
     void Start()
     {
+        videoCanvas.gameObject.SetActive(false);
         sound = GetComponent<AudioSource>();
-        isPressed = false;
-        
+        isPlaying = false;
+        buttonText.text = "START";
     }
 
     private void OnTriggerEnter(Collider other)
     {
-         if (!isPressed) 
+        if (!isPlaying)
         {
-          button.transform.localPosition = new Vector3(0, 0.003f,0);
-            presser = other.gameObject;
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
             onPress.Invoke();
             sound.Play();
-            isPressed = true;
+            StartCanvasVideo(); // Start the video when the button is pressed
+            isPlaying = true;
+            buttonText.text = "STOP";
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == presser)
+        if (isPlaying)
         {
             button.transform.localPosition = new Vector3(0, 0.015f, 0);
             onRelease.Invoke();
-            isPressed=false;
+            StopCanvasVideo(); // Stop the video when the button is released
+            isPlaying = false;
+            buttonText.text = "START";
         }
     }
 
-    public void SpawnSphere()
+    public void StartCanvasVideo()
     {
-         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        sphere.transform.localPosition = new Vector3(0, 1, 2);
-        sphere.AddComponent<Rigidbody>();
+        videoCanvas.gameObject.SetActive(true);
+        videoPlayer.Play();
+        StartCoroutine(WaitForVideoToEnd());
     }
 
+    public void StopCanvasVideo()
+    {
+        videoPlayer.Stop();
+        videoCanvas.gameObject.SetActive(false);
+    }
 
-
+    IEnumerator WaitForVideoToEnd()
+    {
+        while (videoPlayer.isPlaying)
+        {
+            yield return null;
+        }
+        // Video has finished playing, change button text back to "START"
+        buttonText.text = "START";
+    }
 }
