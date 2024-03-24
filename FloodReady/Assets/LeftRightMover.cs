b5211512ace9bdd4f8ce3660477caeba079f6f29
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-24 18:23:52 +0800

    Created logic for Controller Image HTP

---
+++ FloodReady/Assets/LeftRightMover.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class LeftRightMover : MonoBehaviour
+{
+    public GameObject[] objectsToMove;
+    public int currentIndex = 0;
+    public GameObject incrementButton;
+    public GameObject decrementButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveObjects(); // Move objects to initial state
+    }
+
+    // Method to move objects according to the current index
+    void MoveObjects()
+    {
+        // Disable all objects
+        foreach (GameObject obj in objectsToMove)
+        {
+            obj.SetActive(false);
+        }
+
+        // Enable the object at currentIndex
+        objectsToMove[currentIndex].SetActive(true);
+    }
+
+    // Method to increment the index and move objects to the right
+    public void IncrementIndex()
+    {
+        currentIndex = (currentIndex + 1) % objectsToMove.Length;
+        MoveObjects();
+    }
+
+    // Method to decrement the index and move objects to the left
+    public void DecrementIndex()
+    {
+        currentIndex = (currentIndex - 1 + objectsToMove.Length) % objectsToMove.Length;
+        MoveObjects();
+    }
+
+    
+  
+}
