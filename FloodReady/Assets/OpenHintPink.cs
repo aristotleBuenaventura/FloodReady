b4a6522b9cdb6a892411ddb33bea80add18effc7
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-24 15:59:14 +0800

    Hint Clothes icons logic finish

---
+++ FloodReady/Assets/OpenHintPink.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenHintPink : MonoBehaviour
+{
+    public GameObject Pink;
+    public ShowHintCanvasScene3 hintCanvas;
+    private bool canActivate = true; // Flag to track if canvas activation is allowed
+    public TotalPoints points;
+    private bool canDeduct = false;
+    public RecoveryCanvasController RecoveryCanvasController;
+
+    void Start()
+    {
+        Pink.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand") && canActivate)
+        {
+
+            hintCanvas.ShowCloth2Canvas();
+            RecoveryCanvasController.HideAllCanvas();
+            if (!canDeduct)
+            {
+                points.DecrementPoints(200);
+                canDeduct = true;
+            }
+
+        }
+    }
+}
