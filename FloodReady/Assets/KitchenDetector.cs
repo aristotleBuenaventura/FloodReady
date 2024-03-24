aede9da5355159e30492c4ea928749b3283264d1
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-24 17:04:00 +0800

    Kitchen detector test

--- FloodReady/Assets/KitchenDetector.cs
+++ FloodReady/Assets/KitchenDetector.cs
@@ -6,7 +6,7 @@ public class KitchenDetector : MonoBehaviour
     [Header("Teleport Settings")]
     public Vector3 desiredPosition = new Vector3(1.0f, 2.0f, 3.0f);
     public Vector3 desiredRotation = new Vector3(45.0f, 90.0f, 0.0f);
-
+    public GameObject Player;
 
 
 
@@ -17,20 +17,20 @@ public class KitchenDetector : MonoBehaviour
             Debug.Log("Player detected! Teleporting...");
 
 
-            TeleportPlayer(other.gameObject);
+            TeleportPlayer();
         }
     }
 
-    private void TeleportPlayer(GameObject player)
+    private void TeleportPlayer()
     {
-        if (player != null)
+        if (Player != null)
         {
 
             // Set the desired position from the Inspector
-            player.transform.position = desiredPosition;
+            Player.transform.position = desiredPosition;
 
             // Set the desired rotation from the Inspector
-            player.transform.rotation = Quaternion.Euler(desiredRotation);
+            Player.transform.rotation = Quaternion.Euler(desiredRotation);
 
 
         }
