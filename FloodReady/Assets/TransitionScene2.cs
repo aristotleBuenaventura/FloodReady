ef85e798c991497abe8b63580d124ddf7e40701e
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-24 11:17:04 +0800

    Eval room s2 audio button

--- FloodReady/Assets/TransitionScene2.cs
+++ FloodReady/Assets/TransitionScene2.cs
@@ -3,37 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TransitionScene2: MonoBehaviour
+public class TransitionScene2 : MonoBehaviour
 {
-
     public GameObject loadingScreen; // Reference to your loading screen UI element
-    // Start is called before the first frame update
+    public AudioClip oneTimeAudioClip; // Reference to your one-time audio clip
+    private bool soundPlayed = false; // Flag to check if sound has been played
+    private AudioSource audioSource; // Reference to AudioSource component
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Get the AudioSource component attached to the same GameObject
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Hand"))
         {
-            StartCoroutine(LoadScene("Recovery_Resilience"));
-
+            StartCoroutine(LoadSceneWithAudio("Recovery_Resilience"));
         }
     }
 
-    IEnumerator LoadScene(string sceneName)
+    IEnumerator LoadSceneWithAudio(string sceneName)
     {
+        // Play the one-time audio clip if available and not already played
+        if (oneTimeAudioClip != null && audioSource != null && !soundPlayed)
+        {
+            audioSource.PlayOneShot(oneTimeAudioClip);
+            soundPlayed = true; // Set flag to indicate sound has been played
+        }
+
+        yield return new WaitForSeconds(oneTimeAudioClip.length); // Wait for the audio clip to finish
+
         // Activate the loading screen
         loadingScreen.SetActive(true);
 
-        // Load the scene synchronously
+        // Load the scene asynchronously
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
 
         // Wait until the asynchronous scene fully loads
