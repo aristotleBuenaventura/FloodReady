a16e275fd96ce8d31aef5a6db47b2de017dfd3bc
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-03-30 19:21:57 +0800

    yunice guide

---
+++ FloodReady/Assets/Kitchen_Floor_MiniGame.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitchen_Floor_MiniGame : MonoBehaviour
+{
+    private double flooramount;
+    public IconforKF check;
+    private bool isSet = false;
+    public Kitchen_Checklist_MiniGame checklist;
+    public AudioSource Floor_Audio;
+    private bool isPlay = false;
+    public GameObject Wall1;
+    public Material _material;
+
+    public void floor(double cleanAmount)
+    {
+        flooramount = cleanAmount;
+        CalculateTotalCleanAmount();
+    }
+
+
+    private void CalculateTotalCleanAmount()
+    {
+        double totalAmount = flooramount;
+        clean_as_a_whole(totalAmount);
+    }
+
+    public void clean_as_a_whole(double totalAmount)
+    {
+        int totalAmountInt = (int)totalAmount;
+
+        if (totalAmountInt >= 80)
+        {
+            totalAmountInt = 100; // Update totalAmountInt directly to 100
+        }
+
+        CleanAmountManager.UpdateCleanAmount(totalAmountInt); // Update CleanAmountManager after modifying totalAmountInt
+
+        if (totalAmountInt == 100 && !isSet)
+        {
+            Wall1.GetComponent<Renderer>().material = _material;
+            check.SetCheckIconVisible(true);
+            check.SetUncheckIconVisible(false);
+            checklist.checklist[5] = true;
+            if (Floor_Audio != null && !isPlay)
+            {
+                // Play the flush sound
+                Floor_Audio.Play();
+
+                isPlay = true;
+            }
+            isSet = true;
+        }
+    }
+}
