9053f0b9900360611d8e55954f897990c854b395
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-30 21:24:45 +0800

    [S1] Remove all hide canvas in hints

--- FloodReady/Assets/OpenHintClothes.cs
+++ FloodReady/Assets/OpenHintClothes.cs
@@ -8,7 +8,7 @@ public class OpenHintClothes : MonoBehaviour
     private bool canActivate = true; // Flag to track if canvas activation is allowed
     public TotalPoints points;
     private bool canDeduct = false;
-    public CanvasController CanvasController;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,7 @@ public class OpenHintClothes : MonoBehaviour
  
 
             hintCanvas.ShowClothesCanvas();
-        CanvasController.HideAllCanvas();
+   
         if (!canDeduct)
             {
                 points.DecrementPoints(100);
