440431688e6702435819e21876b935820ff98559
Jayjay Naval <jayjaynaval02@gmail.com>
2024-03-30 21:26:42 +0800

    [S3] Remove hide all canvas in clothes

--- FloodReady/Assets/OpenHintBlueDirty.cs
+++ FloodReady/Assets/OpenHintBlueDirty.cs
@@ -9,7 +9,7 @@ public class OpenHintBlueDirty : MonoBehaviour
     private bool canActivate = true; // Flag to track if canvas activation is allowed
     public TotalPoints points;
     private bool canDeduct = false;
-    public RecoveryCanvasController RecoveryCanvasController;
+
 
     void Start()
     {
@@ -22,7 +22,7 @@ public class OpenHintBlueDirty : MonoBehaviour
         {
 
             hintCanvas.ShowCloth4Canvas();
-            RecoveryCanvasController.HideAllCanvas();
+    
             if (!canDeduct)
             {
                 points.DecrementPoints(50);
