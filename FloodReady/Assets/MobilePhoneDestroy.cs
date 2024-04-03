03e7fdf710ff38f2a8a62a4cadae958dd576133a
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-04-03 09:17:44 +0800

    [S2] Successfully attached the phone on hand

---
+++ FloodReady/Assets/MobilePhoneDestroy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePhoneDestroy : MonoBehaviour
+{
+    public OVRGrabber leftGrabber; // Assign the left OVRGrabber component in the Unity Editor
+    public OVRGrabber rightGrabber; // Assign the right OVRGrabber component in the Unity Editor
+    private bool isGrabbed = false;
+    public GameObject MobilePhoneOnHand;
+    public GameObject MobilePhoneOnBag;
+
+    void Start()
+    {
+        MobilePhoneOnBag.SetActive(true);
+        MobilePhoneOnHand.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Check if the user is holding the object using either the left or right OVRGrabber
+        if ((leftGrabber != null && leftGrabber.grabbedObject == GetComponent<OVRGrabbable>()) ||
+            (rightGrabber != null && rightGrabber.grabbedObject == GetComponent<OVRGrabbable>()))
+        {
+            // Deactivate the GameObject with a delay
+            if (!isGrabbed)
+            {
+                StartCoroutine(DisappearWithDelay());
+                isGrabbed = true;
+            }
+        }
+    }
+
+    IEnumerator DisappearWithDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        MobilePhoneOnHand.SetActive(true);
+        MobilePhoneOnBag.SetActive(false);
+    }
+}
--- FloodReady/Assets/circlepoint.cs
+++ FloodReady/Assets/circlepoint.cs
@@ -7,11 +7,15 @@ public class circlepoint : MonoBehaviour
     private bool checklistShown = false;
     public GameObject canvas;
     public CanvasToggle toggle;
+    public GameObject Bag;
+    public GameObject MobilePhoneOnHand;
 
     void Start()
     {
         if (!checklistShown)
         {
+            MobilePhoneOnHand.SetActive(false);
+            Bag.SetActive(false);
             toggle.showChecklist();
             canvas.SetActive(true);
             checklistShown = true;
