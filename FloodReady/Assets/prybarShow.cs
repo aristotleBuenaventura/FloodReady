86801e737cee0fc941b7ddb66e4cca510fa5662a
Jayjay Naval <jayjaynaval02@gmail.com>
2024-04-13 20:44:16 +0800

    [S2] Deleted back button script and fix the errors

--- FloodReady/Assets/prybarShow.cs
+++ FloodReady/Assets/prybarShow.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class PrybarShow : MonoBehaviour
 {
     public EscapeCanvasController escapeCanvasController;
-    private bool hasInteracted = false;
-    public GameObject ToolsRoom;
-
+    private bool hasInteracted = false;   
     void Start()
     {
-        ToolsRoom.SetActive(true);
+    
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,11 +21,6 @@ public class PrybarShow : MonoBehaviour
             // Set the flag to true to indicate that the interaction has occurred
             hasInteracted = true;
 
-            // Corrected syntax to set the active state of ToolsRoom to false
-            ToolsRoom.SetActive(false);
-
-            // Optionally, disable the collider to prevent repeated triggers
-            // GetComponent<Collider>().enabled = false;
         }
     }
 }
