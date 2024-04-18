1a5df45e5691ab12a5b3efcd9b6f9794680de83f
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-04-18 14:11:01 +0800

    [S1] Removed the audio's play on awake

--- FloodReady/Assets/Codes/SceneLoader.cs
+++ FloodReady/Assets/Codes/SceneLoader.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class SceneChanger : MonoBehaviour
 {
     public GameObject loadingScreen;
+    public GameObject loadingScreen2;
+    public Slider loadingSlider;
+    public TextMeshProUGUI progressText;
+
 
     public void MoveToEvacuation_Essential()
     {
@@ -44,12 +49,42 @@ public class SceneChanger : MonoBehaviour
 
     IEnumerator LoadScene(string sceneName)
     {
+        // Activate the loading screen
         loadingScreen.SetActive(true);
+        loadingScreen2.SetActive(true);
+
+        float progress = 0f;
+
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        asyncLoad.allowSceneActivation = false; //scene will not load while this value is false
+
+        while (progress < 1f)
+        {
+            progress += 0.01f;
+
+            // Ensure progress stays within the range [0, 1]
+            progress = Mathf.Clamp01(progress);
+
+            loadingSlider.value = progress;
+
+            // Update the progress text
+            progressText.text = "Loading: " + (progress * 100f).ToString("F0") + "%";
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        // Allow the scene to be activated
+        asyncLoad.allowSceneActivation = true;
+
+        // Wait for the scene to finish loading
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
+
+        // Deactivate the loading screen after the scene is loaded
         loadingScreen.SetActive(false);
+        loadingScreen2.SetActive(false);
     }
 }
