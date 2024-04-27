3a60f865551694f8b42f73ea3df2936729301d82
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2024-04-27 18:16:15 +0800

    [HTP] Added comments in HeadMovementLogic.cs

--- FloodReady/Assets/HeadMovementLogic.cs
+++ FloodReady/Assets/HeadMovementLogic.cs
@@ -27,38 +27,46 @@ public class HeadMovementLogic : MonoBehaviour
         float zRotation = cameraHead.transform.rotation.eulerAngles.z;
         Debug.Log("x: " + xRotation + " y: " + yRotation + " z: " + zRotation);
 
-        // Check the rotation around the z-axis
+        //PITCH
+        // LOOK UP
         if (xRotation >= 300f && xRotation <= 320f)
         {
             LU.SetCheckIconVisible(true);
             LU.SetUncheckIconVisible(false);
         }
-        
-        if (xRotation >= 60f && xRotation <= 75f)
+        //PITCH
+        // LOOK DOWN
+        else if (xRotation >= 60f && xRotation <= 75f)
         {
             LD.SetCheckIconVisible(true);
             LD.SetUncheckIconVisible(false);
         }
-        
-        if (yRotation >= 100f && yRotation <= 110f)
+
+        //YAW
+        //LOOK LEFT
+        else if (yRotation >= 100f && yRotation <= 110f)
         {
             LL.SetCheckIconVisible(true);
             LL.SetUncheckIconVisible(false);
         }
-        
-        if (yRotation >= 250f && yRotation <= 260f)
+        //YAW
+        //LOOK RIGHT
+        else if (yRotation >= 250f && yRotation <= 260f)
         {
             LR.SetCheckIconVisible(true);
             LR.SetUncheckIconVisible(false);
         }
-        
-        if (zRotation >= 70f && zRotation <= 80f)
+
+        //ROLL
+        //TILT TO THE LEFT
+        else if (zRotation >= 70f && zRotation <= 80f)
         {
             TL.SetCheckIconVisible(true);
             TL.SetUncheckIconVisible(false);
         }
-        
-        if (zRotation >= 270f && zRotation <= 300f)
+        //ROLL
+        //TILT TO THE RIGHT
+        else if (zRotation >= 270f && zRotation <= 300f)
         {
             TR.SetCheckIconVisible(true);
             TR.SetUncheckIconVisible(false);
