733770388587a9076bc3a055535e8e35d57caa6b
Adimno <jayjaynaval02@gmail.com>
2024-05-01 20:51:47 +0800

    [s1,s2,s3] changes the script of performance feedback

--- FloodReady/Assets/performanceFeedback.cs
+++ FloodReady/Assets/performanceFeedback.cs
@@ -22,32 +22,32 @@ public class PerformanceFeedback : MonoBehaviour
     {
         int points = totalPointsScript.points;
 
-        if (points >= 13000)
+        if (points >= 130)
         {
             feedbackText.text = "YOU ARE NOW FLOODREADY";
             feedbackText2.text = "Your performance showcases mastery in flood preparedness training";
         }
-        else if (points >= 10000 && points <= 12000)
+        else if (points >= 100 && points <= 120)
         {
             feedbackText.text = "YOU ARE ON THE RIGHT PATH";
             feedbackText2.text = "Your performance indicates progress, but additional effort is required";
         }
-        else if (points >= 8000 && points <= 9000)
+        else if (points >= 80 && points <= 90)
         {
             feedbackText.text = "YOU ARE DOING WELL";
             feedbackText2.text = "Your performance demonstrates noticeable progress in flood response";
         }
-        else if (points >= 6000 && points <= 8000)
+        else if (points >= 60 && points <= 80)
         {
             feedbackText.text = "GOOD BUT NEED MORE PRACTICE";
             feedbackText2.text = "Your performance is acceptable, but lacks depth in flood preparedness";
         }
-        else if (points >= 3000 && points <= 5000)
+        else if (points >= 30 && points <= 50)
         {
             feedbackText.text = "YOU NEED PRACTICE";
             feedbackText2.text = "Immediate improvement is required.";
         }
-        else if (points >= 0 && points <= 2000)
+        else if (points >= 0 && points <= 20)
         {
             feedbackText.text = "YOU ARE NOT FLOODREADY";
             feedbackText2.text = "You failed your training. You need to be floodready!";
